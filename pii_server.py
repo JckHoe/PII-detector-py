13a6b6c678d4cb2294aacf73b162f6c99a667c75
jckhoe <ongjackhoe@gmail.com>
2025-07-16 21:51:43 +0900

    Print the response

--- pii_server.py
+++ pii_server.py
@@ -36,6 +36,8 @@ class ModelHandler(BaseHTTPRequestHandler):
 
                 response = model.infer(input_text)
 
+                print(f"Response : {response}\n")
+
                 self.send_response(200)
                 self.send_header('Content-Type', 'application/json')
                 self.end_headers()
